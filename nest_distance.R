60901d36e4575d97ad1d4f358ab67adf263d0588
Tom Evans <thomas.jude.evans@gmail.com>
2015-01-07 13:31:03 +0100

    Script to calculate the distance to nest for each GPS location. Then output this information to a new DB table

---
+++ nest_distance.R
@@ -0,0 +1,12 @@
+# Annotate GPS positions with distance from nest
+
+# 1. Get required GPS data from DB (device_info_serial, ring_number, date_time,
+# lat, long)
+
+# 2. Get DB table for ring_number + nest info
+
+# 3. Get distance function (saved script - can copy from lbbg project)
+
+# 4. Calculate distance for each GPS
+
+# 5. Put in table and output to DB
