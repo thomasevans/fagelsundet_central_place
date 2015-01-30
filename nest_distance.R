b25c64b859dd3df6e077eda13faf65e1c6080914
Tom Evans <thomas.jude.evans@gmail.com>
2015-01-30 14:20:02 +0100

    New explanatory comments

--- nest_distance.R
+++ nest_distance.R
@@ -1,12 +1,20 @@
 # Annotate GPS positions with distance from nest
 
+
+# Database function -----
+
+# Connect to database
+
+
 # 1. Get required GPS data from DB (device_info_serial, ring_number, date_time,
 # lat, long)
 
-# 2. Get DB table for ring_number + nest info
+# 2. Get DB table for deployment_info (including nest location and island location)
+
+
 
 # 3. Get distance function (saved script - can copy from lbbg project)
 
-# 4. Calculate distance for each GPS
+# 4. Calculate distance for each GPS (from nest and island)
 
 # 5. Put in table and output to DB
